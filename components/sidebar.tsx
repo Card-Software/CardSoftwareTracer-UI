47926077187b03798adfbcc16978a1dada89b5f8
cmshie05 <collinshields35@gmail.com>
2024-05-06 17:16:46 -0400

    working sidebar and page routing functionality

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,18 +1,25 @@
 // components/Sidebar.tsx
+import Link from "next/link";
 import React from "react";
 
 const menuItems = [
   { id: 1, label: "Home", link: "/" },
-  { id: 2, label: "Process Editor", link: "/process-editor" },
-  { id: 3, label: "Dashboard", link: "/dashboard" },
+  { id: 2, label: "Process Editor", link: "/ProcessEditorPage" },
+  { id: 3, label: "Dashboard", link: "/DashboardPage" },
 ];
 
 const Sidebar: React.FC = () => {
   return (
-    <aside className="bg-blue-900 text-white h-full w-32">
+    <aside className="bg-blue-900 text-white flex flex-col sidebar">
       <div className="flex flex-col items-start mt-24">
         {menuItems.map(({ ...menu }) => {
-          return <div>{menu.label}</div>;
+          return (
+            <div>
+              <Link href={menu.link}>
+                <div>{menu.label}</div>
+              </Link>
+            </div>
+          );
         })}
       </div>
     </aside>
--- pages/DashboardPage.tsx
+++ pages/DashboardPage.tsx
@@ -1,1 +1,13 @@
+// components/DashboardPage.js
+import Layout from "@/app/layout";
+import React from "react";
 
+const DashboardPage = () => {
+  return (
+    <Layout>
+      <div>Dashboard</div>
+    </Layout>
+  );
+};
+
+export default DashboardPage;
