d7bfa67e504c0ea05a75af55fba12ac54a5807e2
cmshie05 <collinshields35@gmail.com>
2024-07-01 19:17:25 -0400

    changes to make site consistent and more clean

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -10,35 +10,35 @@ interface MenuItem {
 }
 
 const menuItems: MenuItem[] = [
-  // { id: 1, label: 'Home', link: '/' },
   { id: 2, label: 'Trac. Stream', link: '/TraceabilityStream' },
   { id: 3, label: 'Dashboard', link: '/Dashboard' },
 ];
 
 const Sidebar: React.FC = () => {
-  const fullPath = usePathname() || '/'; // Get the full current path
-  const highestHierarchyPath = '/' + fullPath.split('/')[1]; // Extract the highest hierarchy part
-  const [pathname, setPathname] = useState(highestHierarchyPath); // Initialize state with the highest hierarchy
+  const fullPath = usePathname() || '/';
+  const highestHierarchyPath = '/' + fullPath.split('/')[1];
+  const [pathname, setPathname] = useState(highestHierarchyPath);
 
-  // Use useEffect to update the pathname state whenever the path changes
   useEffect(() => {
-    const highestHierarchy = '/' + fullPath.split('/')[1]; // Extract highest hierarchy
+    const highestHierarchy = '/' + fullPath.split('/')[1];
     console.log('Current path:', fullPath);
     console.log('Highest hierarchy:', highestHierarchy);
-    setPathname(highestHierarchy); // Update state with new highest hierarchy
+    setPathname(highestHierarchy);
     console.log('Pathname after update:', highestHierarchy);
   }, [fullPath]);
 
   return (
-    <aside className="sidebar inline-block h-full w-44 border-e-2 border-white bg-gray-100 text-teal-800">
-      <div className="flex flex-col items-end">
+    <aside className="sidebar flex h-full min-h-screen w-44 flex-col border-e-2 border-white bg-gray-100 text-teal-700">
+      <div className="flex flex-grow flex-col items-end">
         {menuItems.map((menu) => {
           const isActive = highestHierarchyPath === menu.link;
           return (
             <Link key={menu.id} href={menu.link} style={{ width: '100%' }}>
               <div
                 className={`px-4 py-2 text-right ${
-                  isActive ? 'bg-teal-800 text-white' : 'hover:bg-teal-500'
+                  isActive
+                    ? 'bg-teal-700 text-white'
+                    : 'hover:bg-teal-600 hover:text-white'
                 }`}
               >
                 {menu.label}
