495bfb337787b38ebf766acd1be3ee5318ec54d3
Jose David Soto Puac <jsoto-2019315@kinal.edu.gt>
2024-08-23 17:00:07 -0600

    Implements axiosInstance to file-management.proxy

--- proxies/email.proxy.ts
+++ proxies/email.proxy.ts
@@ -2,24 +2,6 @@ import { ProductOrderCreatedEmail, PoStatusChanged } from '@/models/email';
 import axiosInstance from '@/utils/axiosInstance';
 
 class EmailProxy {
-  private baseUrl: string = process.env.NEXT_PUBLIC_TRACER_APP_API_URL || '';
-
-  async EmailPoStatusUpdate(
-    recipient: string,
-    poNumber: string,
-    firstName: string,
-  ): Promise<Response> {
-    try {
-      const response = await axiosInstance.post('Email/EmailPO', {
-        recipient,
-        poNumber,
-        firstName,
-      });
-      return response.data;
-    } catch (error) {
-      throw new Error('An error occurred while sending the email');
-    }
-  }
 
   async EmailPoCreation(body: ProductOrderCreatedEmail): Promise<Response> {
     try {
