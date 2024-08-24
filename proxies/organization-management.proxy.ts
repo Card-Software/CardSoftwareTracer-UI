8fea599f2cadbe04f05f3ab90003af52a9bd666c
Jose David Soto Puac <jsoto-2019315@kinal.edu.gt>
2024-08-23 19:10:48 -0600

    Implement axiosInstance in team-label.proxy

--- proxies/organization-management.proxy.ts
+++ proxies/organization-management.proxy.ts
@@ -3,8 +3,6 @@ import { User } from '@/models/user';
 import axiosInstance from '@/utils/axiosInstance';
 
 class OrganizationManagementProxy {
-  private baseUrl = process.env.NEXT_PUBLIC_TRACER_APP_API_URL;
-
   //#region
   // Organization controller
   async GetOrganization(id: string): Promise<Organization> {
