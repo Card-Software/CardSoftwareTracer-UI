5596edc881b769ea84af504aca3def3991a4a6ea
Jose David Soto Puac <jsoto-2019315@kinal.edu.gt>
2024-08-23 19:19:05 -0600

    Implement axiosInstance in user.proxy

--- proxies/user-authorization.proxy.ts
+++ proxies/user-authorization.proxy.ts
@@ -1,11 +1,11 @@
 import { Group } from '@/models/group';
 import axiosInstance from '@/utils/axiosInstance';
 class UserAuthorizationProxy {
-  private baseUrl: string = process.env.NEXT_PUBLIC_TRACER_APP_API_URL || '';
-
   async getAllGroups(ownerRef: string): Promise<Group[]> {
     try {
-      const response = await axiosInstance.get(`GroupsController/${ownerRef}/all`);
+      const response = await axiosInstance.get(
+        `GroupsController/${ownerRef}/all`,
+      );
       return response.data;
     } catch (error) {
       console.log('error', error);
--- proxies/user.proxy.ts
+++ proxies/user.proxy.ts
@@ -1,28 +1,18 @@
 import { User } from '@/models/user';
-
+import axiosInstance from '@/utils/axiosInstance';
 class UserProxy {
   private baseUrl: string = process.env.NEXT_PUBLIC_TRACER_APP_API_URL || '';
 
   async updateUser(user: User): Promise<void> {
-    const response = await fetch(
-      `${this.baseUrl}UserController/update/${user.id}`,
-      {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-      },
-    );
-
-    if (!response.ok) {
-      const errorMessage = await response.text();
-      throw new Error(`Failed to update user: ${errorMessage}`);
-    }
-
-    // If the server returns a 204 No Content, there's no body to parse
-    if (response.status !== 204) {
-      return await response.json();
+    try {
+      const response = await axiosInstance.put(
+        `UserController/update/${user.id}`,
+        user,
+      );
+      return response.data;
+    } catch (error) {
+      console.log('error', error);
+      throw error;
     }
   }
 }
