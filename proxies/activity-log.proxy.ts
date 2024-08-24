8b3cc2fec1709a9302ee6fa0de2d92d33b8dc0d0
Jose David Soto Puac <jsoto-2019315@kinal.edu.gt>
2024-08-23 18:55:05 -0600

    Implement axiosInstance in organization-management.proxy

--- proxies/activity-log.proxy.ts
+++ proxies/activity-log.proxy.ts
@@ -23,50 +23,9 @@ class ActivityLogProxy {
       );
       return response.data;
     } catch (error: any) {
-      // console.error('An error occurred while fetching the activity log:', error);
       return [];
     }
   }
 }
 
 export const activityLogProxy = new ActivityLogProxy();
-
-// import { ActivityLog } from '@/models/activity-log';
-// import { Group } from '@/models/group';
-// import { S3ObjectDto } from '@/models/s3-object-dto';
-
-// class ActivityLogProxy {
-//   private baseUrl: string = process.env.NEXT_PUBLIC_TRACER_APP_API_URL || '';
-
-//   async insertActivityLog(activityLog: ActivityLog): Promise<boolean> {
-//     const response = await fetch(
-//       `${this.baseUrl}ActivityLogController/insert`,
-//       {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(activityLog),
-//       },
-//     );
-//     return response.ok;
-//   }
-
-//   async getActivityLogByPo(po: string): Promise<ActivityLog[]> {
-//     const response = await fetch(
-//       `${this.baseUrl}ActivityLogController/getbyPo/${po}`,
-//       {
-//         method: 'GET',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//       },
-//     );
-//     if (response.ok) {
-//       return await response.json();
-//     }
-//     return [];
-//   }
-// }
-
-// export const activityLogProxy = new ActivityLogProxy();
--- proxies/organization-management.proxy.ts
+++ proxies/organization-management.proxy.ts
@@ -7,86 +7,53 @@ class OrganizationManagementProxy {
 
   //#region
   // Organization controller
-
   async GetOrganization(id: string): Promise<Organization> {
-    const response = await fetch(
-      `${this.baseUrl}OrganizationController/get/${id}`,
-    );
-    return await response.json();
-  }
-
-  async UpdateOrganization(organization: Organization): Promise<Organization> {
-    const response = await fetch(
-      `${this.baseUrl}OrganizationController/update/${organization.id}`,
-      {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(organization),
-      },
-    );
-    return await response.json();
-  }
-
-  async GetAllEnterprises(): Promise<Organization[]> {
-    const response = await fetch(`${this.baseUrl}OrganizationController/all`);
-    return await response.json();
-  }
-
-  async DeleteEnterprise(id: string): Promise<Organization> {
-    const response = await fetch(
-      `${this.baseUrl}OrganizationController/delete/${id}`,
-      {
-        method: 'DELETE',
-      },
-    );
-    return await response.json();
+    try {
+      const response = await axiosInstance.get(
+        `OrganizationController/get/${id}`,
+      );
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }
   //#endregion
 
   //#region
   // User controller
-  async CreateUser(user: User): Promise<User> {
-    const response = await fetch(`${this.baseUrl}UserController/create`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(user),
-    });
-    return await response.json();
-  }
 
   async GetUser(id: string): Promise<User> {
-    const response = await fetch(`${this.baseUrl}UserController/get/${id}`);
-    return await response.json();
+    try {
+      const response = await axiosInstance.get(`UserController/get/${id}`);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }
 
   async UpdateUser(user: User): Promise<User> {
-    const response = await fetch(
-      `${this.baseUrl}UserController/update/${user.id}`,
-      {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-      },
-    );
-    return await response.json();
+    try {
+      const response = await axiosInstance.put(
+        `UserController/update/${user.id}`,
+        user,
+      );
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }
 
   async GetAllUsers(): Promise<User[]> {
-    const response = await fetch(`${this.baseUrl}UserController/all`);
-    return await response.json();
-  }
-
-  async DeleteUser(id: string): Promise<User> {
-    const response = await fetch(`${this.baseUrl}UserController/delete/${id}`, {
-      method: 'DELETE',
-    });
-    return await response.json();
+    try {
+      const response = await axiosInstance.get('UserController/all');
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }
 }
 
