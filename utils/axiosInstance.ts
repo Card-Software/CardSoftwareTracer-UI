31695c973eeb1e26437401b1d7e20152fcc166a2
Jose David Soto Puac <jsoto-2019315@kinal.edu.gt>
2024-08-26 14:28:38 -0600

    fix problems with token

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -6,10 +6,12 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('CSTracerUserJWT');
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
     }
+
+    console.log('token in header confirmation', config.headers['Authorization']);
     return config;
   },
   (error) => {
