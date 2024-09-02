55348a03c4dd88e956561e0a57facb1037da8596
Jose David Soto Puac <jsoto-2019315@kinal.edu.gt>
2024-09-02 17:34:54 -0600

    Note component finished

--- components/modals/notes-modal.component.tsx
+++ components/modals/notes-modal.component.tsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import BaseModal from '../_base/base-modal.component';
 import { Note } from '@/models/note';
 import { User } from '@/models/user';
+import '@/styles/notes.css';
 
 interface NotesModalProps {
   isOpen: boolean;
   onClose: () => void;
   notes: Note[];
   setNotes: React.Dispatch<React.SetStateAction<Note[]>>;
-  currentUser: User; // Añadido para determinar el usuario que está añadiendo la nota
+  currentUser: User;
 }
 
 const NotesModal: React.FC<NotesModalProps> = ({
@@ -19,6 +20,11 @@ const NotesModal: React.FC<NotesModalProps> = ({
   currentUser,
 }) => {
   const [newNoteContent, setNewNoteContent] = useState('');
+  const [expandedNoteIndex, setExpandedNoteIndex] = useState<number | null>(
+    null,
+  );
+
+  const contentRefs = useRef<(HTMLDivElement | null)[]>([]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setNewNoteContent(e.target.value);
@@ -31,10 +37,21 @@ const NotesModal: React.FC<NotesModalProps> = ({
         enteredBy: currentUser,
         dateEntered: new Date(),
       };
-      setNotes([...notes, newNote]); // Actualiza el array de notas
-      setNewNoteContent(''); // Limpia el campo de texto
+      setNotes([...notes, newNote]);
+      setNewNoteContent('');
     }
   };
+
+  const toggleNoteExpansion = (index: number) => {
+    setExpandedNoteIndex(expandedNoteIndex === index ? null : index);
+  };
+
+  useEffect(() => {
+    if (expandedNoteIndex !== null && contentRefs.current[expandedNoteIndex]) {
+      const element = contentRefs.current[expandedNoteIndex];
+      element!.style.maxHeight = `${element!.scrollHeight}px`;
+    }
+  }, [expandedNoteIndex]);
 
   return (
     <BaseModal
@@ -43,8 +60,8 @@ const NotesModal: React.FC<NotesModalProps> = ({
       isOpen={isOpen}
       loading={false}
       typeSaveButton="button"
-      onSave={handleSaveNote} // Vincula el botón de guardar al manejador de guardado
-      canSave={newNoteContent.trim().length > 0} // Deshabilita el botón de guardar si no hay contenido
+      onSave={handleSaveNote}
+      canSave={newNoteContent.trim().length > 0}
     >
       <div className="mt-4">
         <label
@@ -62,9 +79,44 @@ const NotesModal: React.FC<NotesModalProps> = ({
           placeholder="Write your note here..."
         />
       </div>
-      <div>
+
+      <div className="mt-4">
         {notes.map((note, index) => (
-          <p key={index}>{note.content}</p>
+          <div key={index} className="mb-2">
+            <button
+              className={`flex w-full items-center justify-between rounded-lg px-4 py-2 text-left ${
+                expandedNoteIndex === index ? 'bg-gray-300 ' : 'bg-transparent'
+              }`}
+              onClick={() => toggleNoteExpansion(index)}
+            >
+              <span className="flex-1 truncate">{note.content}</span>
+              <span className="ml-4 text-xl">
+                {expandedNoteIndex === index ? '-' : '+'}
+              </span>
+            </button>
+            <div
+              ref={(el) => {
+                contentRefs.current[index] = el;
+              }}
+              className={`transition-max-height relative overflow-hidden duration-500 ease-in-out`}
+              style={{
+                maxHeight:
+                  expandedNoteIndex === index
+                    ? `${contentRefs.current[index]?.scrollHeight}px`
+                    : '0px',
+              }}
+            >
+              <div className="mt-2 rounded-b-lg bg-white p-4 text-gray-700">
+                <div className="max-h-20 overflow-hidden">
+                  <p>{note.content}</p>
+                </div>
+                <p className="mt-2 text-sm text-gray-600">
+                  {note.dateEntered.toString().split('T')[0].replace(/-/g, '/')}{' '}
+                  • {note.enteredBy.firstName} {note.enteredBy.lastname}
+                </p>
+              </div>
+            </div>
+          </div>
         ))}
       </div>
     </BaseModal>
