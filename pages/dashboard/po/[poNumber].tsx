b92175b4af05aacd0e46b1ca45d1e1480545d184
cmshie05 <collinshields35@gmail.com>
2024-09-19 22:55:08 -0400

    changed arrow

--- pages/dashboard/po/[poNumber].tsx
+++ pages/dashboard/po/[poNumber].tsx
@@ -794,9 +794,21 @@ const PurchaseOrderPage: React.FC = () => {
                           </CardDetails>
                         </SectionCard>
                         {secIndex < stream.sections.length - 1 && (
-                          <ArrowIcon>
-                            <FaArrowRight size={24} />
-                          </ArrowIcon>
+                          <svg
+                            width="9"
+                            height="15"
+                            viewBox="0 0 9 15"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M2 2L7.5 7.5L2 13"
+                              stroke="#8D8D8D"
+                              stroke-width="2.5"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            />
+                          </svg>
                         )}
                       </React.Fragment>
                     ))}
