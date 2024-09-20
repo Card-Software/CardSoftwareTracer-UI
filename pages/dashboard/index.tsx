6e721f4f8e0954968ea8d28a3df00b6a6204ee95
cmshie05 <collinshields35@gmail.com>
2024-09-19 21:13:18 -0400

    delete apply filter

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -473,12 +473,6 @@ const Dashboard: React.FC = () => {
                 >
                   Cancel All
                 </button>
-                <button
-                  onClick={applyFilters}
-                  className="rounded-md bg-blue-500 px-5 py-2 font-semibold text-white hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50"
-                >
-                  Apply Filters
-                </button>
               </div>
             </div>
           </div>
