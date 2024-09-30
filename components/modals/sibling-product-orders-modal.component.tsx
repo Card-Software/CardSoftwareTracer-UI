79dff05ce7631fb303240778114896f122505e5d
jonathanh25097 <140763343+jonathanh25097@users.noreply.github.com>
2024-09-30 14:22:39 -0400

    Fix button (#48) (#49)
    * fix save button in Sibling Product Orders modal
    
    * fixed build
    
    ---------
    
    Co-authored-by: José Soto <jsoto-2019315@kinal.edu.gt>

--- components/modals/sibling-product-orders-modal.component.tsx
+++ components/modals/sibling-product-orders-modal.component.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { FaTimes } from 'react-icons/fa';
 import { SiblingProductOrder } from '@/models/sibling-product-order';
 import * as Yup from 'yup';
 import { useForm, useFieldArray, Controller } from 'react-hook-form';
@@ -63,7 +62,7 @@ const SiblingProductOrderModal: React.FC<sibPosProps> = ({
   return (
     <BaseModal
       isOpen={true}
-      onSave={() => handleSubmit(onSubmit)}
+      onSave={handleSubmit(onSubmit)}
       onClose={onClose}
       title="Sibling Product Orders"
       loading={false}
@@ -208,6 +207,7 @@ const RemoveButton = styled.button`
   position: absolute;
   top: 0.5rem;
   right: 0.5rem;
+
   background: none;
   border: none;
   color: #f56565; /* Tailwind text-red-500 */
