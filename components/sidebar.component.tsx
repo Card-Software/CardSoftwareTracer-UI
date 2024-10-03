0cdcde4e748071ea90de44340f2083eaaad9f913
Jose David Soto Puac <jsoto-2019315@kinal.edu.gt>
2024-10-02 20:18:50 -0600

    Refactor sidebar component to support sub-items and expand/collapse functionality

--- components/sidebar.component.tsx
+++ components/sidebar.component.tsx
@@ -2,18 +2,28 @@
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'; // Importing icons for toggle button
+import { FaChevronDown, FaChevronLeft, FaChevronRight } from 'react-icons/fa'; // Importing icons for toggle button
 
 interface MenuItem {
   id: number;
   label: string;
-  link: string;
+  link?: string;
+  subItems?: MenuItem[];
 }
 
 const menuItems: MenuItem[] = [
   { id: 2, label: 'Trac. Stream', link: '/traceability-stream' },
   { id: 3, label: 'Dashboard', link: '/dashboard' },
   { id: 4, label: 'Man. Dashboard', link: '/manager-dashboard' },
+  {
+    id: 5,
+    label: 'Groups',
+    subItems: [
+      { id: 6, label: 'Groups', link: '/group1' },
+      { id: 7, label: 'Team Labels', link: '/group2' },
+      { id: 8, label: 'Statuses', link: '/group3' },
+    ],
+  },
 ];
 
 const Sidebar: React.FC = () => {
@@ -21,6 +31,9 @@ const Sidebar: React.FC = () => {
   const highestHierarchyPath = '/' + fullPath.split('/')[1];
   const [pathname, setPathname] = useState(highestHierarchyPath);
   const [isCollapsed, setIsCollapsed] = useState(false); // State for collapse
+  const [expandedItems, setExpandedItems] = useState<{
+    [key: number]: boolean;
+  }>({}); // State for expand
 
   useEffect(() => {
     const highestHierarchy = '/' + fullPath.split('/')[1];
@@ -29,6 +42,13 @@ const Sidebar: React.FC = () => {
 
   const toggleCollapse = () => {
     setIsCollapsed((prevState) => !prevState);
+  };
+
+  const toggleExpand = (id: number) => {
+    setExpandedItems((prevState) => ({
+      ...prevState,
+      [id]: !prevState[id],
+    }));
   };
 
   return (
@@ -110,22 +130,144 @@ const Sidebar: React.FC = () => {
       <div className="flex flex-grow flex-col items-start">
         {menuItems.map((menu) => {
           const isActive = highestHierarchyPath === menu.link;
+          const isExpanded = expandedItems[menu.id];
           return (
-            <Link key={menu.id} href={menu.link} style={{ width: '100%' }}>
+            <div key={menu.id} style={{ width: '100%' }}>
+              {menu.link ? (
+                <Link href={menu.link}>
+                  <div
+                    className={`px-4 py-2 ${
+                      isActive
+                        ? 'bg-gray-200 font-bold text-black'
+                        : 'hover:bg-gray-500 hover:text-white'
+                    } ${isCollapsed ? 'w-0 opacity-0' : 'w-full opacity-100'} rounded-md transition-all duration-700 ease-in-out`}
+                  >
+                    {menu.label}
+                  </div>
+                </Link>
+              ) : (
+                <div
+                  className={`cursor-pointer px-4 py-2 ${isCollapsed ? 'w-0 opacity-0' : 'w-full opacity-100'} rounded-md transition-all duration-700 ease-in-out`}
+                  onClick={() => toggleExpand(menu.id)}
+                >
+                  <div className="flex items-center justify-between">
+                    <span>{menu.label}</span>
+                    <span
+                      className={`transition-transform duration-300 ease-in-out ${
+                        isExpanded ? 'rotate-180' : 'rotate-0'
+                      }`}
+                    >
+                      <FaChevronDown size={12} color="gray" />
+                    </span>
+                  </div>
+                </div>
+              )}
+
               <div
-                className={`px-4 py-2 ${
-                  isActive
-                    ? 'bg-gray-200 font-bold text-black'
-                    : 'hover:bg-gray-500 hover:text-white'
-                } ${isCollapsed ? 'w-0 opacity-0' : 'w-full opacity-100'} rounded-md transition-all duration-300 ease-in-out`}
+                className={`ml-5 mr-3 overflow-hidden transition-all duration-700 ease-in-out`}
+                style={{
+                  maxHeight: isExpanded
+                    ? `${(menu.subItems?.length || 0) * 48}px`
+                    : '0px',
+                }}
               >
-                {menu.label}
+                {menu.subItems &&
+                  menu.subItems.map((subItem) => (
+                    <Link key={subItem.id} href={subItem.link || '#'}>
+                      <div
+                        className={`px-4 py-2 ${
+                          pathname === subItem.link
+                            ? 'bg-gray-200 font-bold text-black'
+                            : 'hover:bg-gray-500 hover:text-white'
+                        } rounded-md transition-all duration-300 ease-in-out`}
+                      >
+                        {subItem.label}
+                      </div>
+                    </Link>
+                  ))}
               </div>
-            </Link>
+            </div>
           );
         })}
       </div>
     </aside>
+
+    // <aside
+    //   className={`sidebar flex h-full min-h-screen ${
+    //     isCollapsed ? 'w-16' : 'w-44'
+    //   } flex-col border-e-2 border-white bg-gray-100 text-black transition-all duration-300 ease-in-out`}
+    // >
+    //   {/* Toggle Button */}
+    //   <div className="flex justify-start p-2">
+    //     <button
+    //       onClick={toggleCollapse}
+    //       className="flex items-center justify-center rounded border-none p-2 shadow-none transition "
+    //     >
+    //       {isCollapsed ? (
+    //         <FaChevronRight />
+    //       ) : (
+    //         <FaChevronLeft />
+    //       )}
+    //     </button>
+    //   </div>
+
+    //   <div className="flex flex-grow flex-col items-start">
+    //     {menuItems.map((menu) => {
+    //       const isActive = highestHierarchyPath === menu.link;
+    //       const isExpanded = expanded[menu.id];
+
+    //       return (
+    //         <div key={menu.id} style={{ width: '100%' }}>
+    //           {/* Render parent menu items */}
+    //           {menu.link ? (
+    //             <Link href={menu.link}>
+    //               <div
+    //                 className={`px-4 py-2 ${
+    //                   isActive
+    //                     ? 'bg-gray-200 font-bold text-black'
+    //                     : 'hover:bg-gray-500 hover:text-white'
+    //                 } ${isCollapsed ? 'w-0 opacity-0' : 'w-full opacity-100'} rounded-md transition-all duration-300 ease-in-out`}
+    //               >
+    //                 {menu.label}
+    //               </div>
+    //             </Link>
+    //           ) : (
+    //             <div
+    //               className={`px-4 py-2 cursor-pointer ${
+    //                 isCollapsed ? 'w-0 opacity-0' : 'w-full opacity-100'
+    //               } rounded-md transition-all duration-300 ease-in-out`}
+    //               onClick={() => toggleExpand(menu.id)}
+    //             >
+    //               <div className="flex items-center justify-between">
+    //                 <span>{menu.label}</span>
+    //                 {isExpanded ? <FaChevronDown /> : <FaChevronRight />}
+    //               </div>
+    //             </div>
+    //           )}
+
+    //           {/* Render sub-items if they exist and are expanded */}
+    //           {isExpanded && menu.subItems && (
+    //             <div className={`pl-4 ${isCollapsed ? 'hidden' : 'block'}`}>
+    //               {menu.subItems.map((subItem) => (
+    //                 <Link key={subItem.id} href={subItem.link || '#'}>
+    //                   <div
+    //                     className={`px-4 py-2 ${
+    //                       pathname === subItem.link
+    //                         ? 'bg-gray-200 font-bold text-black'
+    //                         : 'hover:bg-gray-500 hover:text-white'
+    //                     } rounded-md transition-all duration-300 ease-in-out`}
+    //                   >
+    //                     {subItem.label}
+    //                   </div>
+    //                 </Link>
+    //               ))}
+    //             </div>
+    //           )}
+    //         </div>
+    //       );
+    //     })}
+    //   </div>
+    // </aside>
   );
 };
 
